5838a68a76e95ef45c646bada4fb2c7db4ba0f5e
WalkerAlone764 <109795525+WalkerAlone764@users.noreply.github.com>
2022-09-13 10:15:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import SignInScreen from './Screens/SignInScreen';
+import SignUp from './Screens/SignUp';
+
+function App() {
+  return <SignUp />;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default App;
